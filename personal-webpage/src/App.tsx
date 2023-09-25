91592d505c8ee7615e66eb340ae47bf903652100
daniel-smith <daniel.smith@xero.com>
2023-09-25 20:01:40 +1300

    Initial ts react app

---
+++ personal-webpage/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Lead Engineer @ Xero
+        </p>
+        <a
+          className="App-link"
+          href="https://medium.com/@daniel.leon.smith"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Medium
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
